c00fc9778d5e8ec9b59cdd80b3432f71c3989a1b
OthelloEngineer <jolee18@student.sdu.dk>
2024-12-15 12:39:45 +0100

    integrate da protobuff

--- protobuf/protogen.py
+++ protobuf/protogen.py
@@ -1,23 +1,21 @@
 import os
 import subprocess
 
-# Path to the .proto file
 proto_file = "playlist.proto"
 current_dir = os.path.dirname(os.path.abspath(__file__))
 proto_file_path = os.path.join(current_dir, proto_file)
 
-# Output directory for the generated Python file
 output_dir = os.path.abspath(os.path.join(current_dir, "../spotify-api"))
 
-# Ensure the output directory exists
-os.makedirs(output_dir, exist_ok=True)
-
-# Command to generate Python classes using protoc
 protoc_command = [
     "protoc", 
-    f"--proto_path={current_dir}",  # Directory of the .proto file
-    f"--python_out={output_dir}",  # Directory for the generated Python file
-    proto_file_path               # The .proto file to process
+    f"--proto_path={current_dir}",  
+    f"--python_out={output_dir}", # python_out
+    f"--pyi_out={output_dir}", # python stubs
+    # java_out
+    f"--java_out={output_dir}",
+    
+    proto_file_path               
 ]
 
 try:
