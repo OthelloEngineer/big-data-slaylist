6756c5499321e841b09de51a3dff8eea17e30634
OFHansen <ohans21@student.sdu.dk>
2024-12-29 20:08:46 +0100

    Deployed dashboard to cluster

--- playlist-dashboard/src/services/apiService.js
+++ playlist-dashboard/src/services/apiService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:8080/catalouge";
+const API_BASE_URL = "catalougeapi:8080/catalouge";
 
 /**
  * Fetch playlists based on genres.
